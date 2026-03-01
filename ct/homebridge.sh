f247af2c10a6522c75a02568847f10d2543d3c34
Wayne Hagan <47152538+owhagan@users.noreply.github.com>
2026-02-28 19:27:03 -0700

    Change source URL for build function
    Update source URL for build function in homebridge.sh

--- ct/homebridge.sh
+++ ct/homebridge.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://github.com/owhagan/Proxmox/blob/5cb6ec32442a845465f2c63794bb4a67d253dc21/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
